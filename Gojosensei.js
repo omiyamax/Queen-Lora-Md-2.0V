9eb75edda931042e913b3373117487209b9ecd40
Omindu Anjana <93399269+omigetapi@users.noreply.github.com>
2022-08-12 22:22:35 +0530

    Update Gojosensei.js

--- Gojosensei.js
+++ Gojosensei.js
@@ -180,10 +180,10 @@ module.exports = GojoMdNx = async (GojoMdNx, m, chatUpdate, store) => {
             if (typeof chats !== 'object') global.db.data.chats[m.chat] = {}
             if (chats) {
                 if (!('mute' in chats)) chats.mute = false
-                if (!('antilink' in chats)) chats.antilink = false
+                if (!('antilink' in chats)) chats.antilink = true
             } else global.db.data.chats[m.chat] = {
                 mute: false,
-                antilink: false,
+                antilink: true,
             }
 		
 	    let setting = global.db.data.settings[botNumber]
